aa248a7e3351c898df4bb881fb378fbaa19df696
rugallord <38218990+rugallord@users.noreply.github.com>
2018-04-20 19:21:35 -0300

    Update RugalVaper.lua

--- RugalVaper.lua
+++ RugalVaper.lua
@@ -1409,6 +1409,161 @@ function Vladimir:Draw()
     end
 end
 
+class "Warwick"
+
+function Warwick:__init()
+	self:LoadSpells()
+	self:LoadMenu()
+	Callback.Add("Tick", function() self:Tick() end)
+	Callback.Add("Draw", function() self:Draw() end)
+end
+
+function Warwick:LoadSpells()
+    Q = { Range = 350}
+    E = { Range = 375}
+    R = { Range = 0, Delay = 0.1, Speed = 1800, Width = 45}
+end
+
+function Warwick:LoadMenu()
+	Warwick = MenuElement({type = MENU, id = "Warwick", name = "Rugal Vaper - Warwick"})
+    
+    Warwick:MenuElement({id = "Enable", name = "Enable Hero", value = true})
+
+    Warwick:MenuElement({type = MENU, id = "Combo", name = "Combo"})
+    Warwick.Combo:MenuElement({id = "Q", name = "Q - Jaws of the Beast", value = true})
+    Warwick.Combo:MenuElement({id = "E", name = "E - Primal Howl", value = true})
+    Warwick.Combo:MenuElement({id = "R", name = "R - Infinite Duress", value = true})
+    
+    Warwick:MenuElement({type = MENU, id = "Harass", name = "Harass"})
+    Warwick.Harass:MenuElement({id = "T", name = "Toggle Spells", key = string.byte("S"), toggle = true, value = true})
+    Warwick.Harass:MenuElement({id = "Q", name = "Q - Jaws of the Beast", value = true})
+    Warwick.Harass:MenuElement({id = "E", name = "E - Primal Howl", value = true})
+    Warwick.Harass:MenuElement({id = "R", name = "R - Infinite Duress", value = true})
+    
+    Warwick:MenuElement({type = MENU, id = "Clear", name = "Clear"})
+    Warwick.Clear:MenuElement({id = "T", name = "Toggle Spells", key = string.byte("A"), toggle = true, value = true})
+    Warwick.Clear:MenuElement({id = "Q", name = "Q - Jaws of the Beast", value = true})
+    
+    Warwick:MenuElement({type = MENU, id = "Draw", name = "Drawings"})
+    Warwick.Draw:MenuElement({id = "Q", name = "Q Range", value = true})
+    Warwick.Draw:MenuElement({id = "E", name = "E Range", value = true})
+    Warwick.Draw:MenuElement({id = "R", name = "R Range", value = true})
+    Warwick.Draw:MenuElement({id = "Harass", name = "Harass Status", type = MENU})
+    Warwick.Draw.Harass:MenuElement({id = "Text", name = "Text Enabled", value = true})
+    Warwick.Draw.Harass:MenuElement({id = "Size", name = "Text Size", value = 10, min = 1, max = 100})
+    Warwick.Draw.Harass:MenuElement({id = "xPos", name = "Text X Position", value = -50, min = -1000, max = 1000, step = 10})
+    Warwick.Draw.Harass:MenuElement({id = "yPos", name = "Text Y Position", value = -140, min = -1000, max = 1000, step = 10})
+    Warwick.Draw:MenuElement({id = "Clear", name = "Clear Status", type = MENU})
+    Warwick.Draw.Clear:MenuElement({id = "Text", name = "Text Enabled", value = true})
+    Warwick.Draw.Clear:MenuElement({id = "Size", name = "Text Size", value = 10, min = 1, max = 100})
+    Warwick.Draw.Clear:MenuElement({id = "xPos", name = "Text X Position", value = -50, min = -1000, max = 1000, step = 10})
+    Warwick.Draw.Clear:MenuElement({id = "yPos", name = "Text Y Position", value = -130, min = -1000, max = 1000, step = 10})
+end
+
+function Warwick:Tick()
+    if not Warwick.Enable:Value() then return end
+    self:Rrange()
+    local mode = GetMode()
+    if mode == "Combo" then
+        local Rtarget = GetTarget(R.Range)
+        if Rtarget and Warwick.Combo.R:Value() then
+            self:CastR(Rtarget)
+        end
+        local Etarget = GetTarget(E.Range) 
+        if Etarget and Warwick.Combo.E:Value() then
+            self:CastE()
+        end
+        local Qtarget = GetTarget(Q.Range)
+        if Qtarget and Warwick.Combo.Q:Value() then
+            self:CastQ(Qtarget)
+        end
+    end
+    if mode == "Harass" and Warwick.Harass.T:Value() then
+        local Rtarget = GetTarget(R.Range)
+        if Rtarget and Warwick.Harass.R:Value() then
+            self:CastR(Rtarget)
+        end
+        local Etarget = GetTarget(E.Range) 
+        if Etarget and Warwick.Harass.E:Value() then
+            self:CastE()
+        end
+        local Qtarget = GetTarget(Q.Range)
+        if Qtarget and Warwick.Harass.Q:Value() then
+            self:CastQ(Qtarget)
+        end
+    end
+    if mode == "Clear" and Warwick.Clear.T:Value() then
+        local Qtarget = GetClearMinion(Q.Range)
+        if Qtarget and Warwick.Clear.Q:Value() then
+            self:CastQ(Qtarget)
+        end
+    end
+end
+
+function Warwick:CastR(target,precision)
+    local precision = precision or 1
+	if Ready(_R) and IsUp(_R) then
+        if target and HPred:CanTarget(target) then
+            local hitChance, aimPosition = HPred:GetHitchance(myHero.pos, target, R.Range, R.Delay, R.Speed, R.Width, false, nil)
+            if hitChance and hitChance >= precision and HPred:GetDistance(myHero.pos, aimPosition) <= R.Range then
+                EnableOrb(false)
+                Control.CastSpell(HK_R, aimPosition)
+                EnableOrb(true)
+            end
+        end
+    end
+end
+
+function Warwick:Rrange()
+    R.Range = myHero.ms * 2.50 - 275
+end
+
+function Warwick:CastQ(target)
+	if Ready(_Q) and IsUp(_Q) then
+        EnableOrb(false)
+        Control.CastSpell(HK_Q,target)
+        EnableOrb(true)
+    end
+end
+
+function Warwick:CastE()
+    if Ready(_E) and IsUp(_E) then
+        EnableOrb(false)
+        Control.CastSpell(HK_E)
+        EnableOrb(true)
+    end
+end
+
+function Warwick:Draw()
+    if not Warwick.Enable:Value() then return end
+    local target = GetTarget(2000)
+    if Warwick.dead then return end
+    if Warwick.Draw.Q:Value() and Ready(_Q) then Draw.Circle(myHero.pos, Q.Range, 3,  Draw.Color(255, 000, 000, 255)) end
+    if Warwick.Draw.E:Value() and Ready(_E) then Draw.Circle(myHero.pos, E.Range, 3,  Draw.Color(255, 255, 255, 000)) end
+    if Warwick.Draw.R:Value() and Ready(_R) then Draw.Circle(myHero.pos, R.Range, 3,  Draw.Color(255, 255, 000, 000)) end
+    local textPos = myHero.pos:To2D()
+    if Warwick.Draw.Harass.Text:Value() then
+        local size = Warwick.Draw.Harass.Size:Value()
+	    local xPos = Warwick.Draw.Harass.xPos:Value()
+	    local yPos = Warwick.Draw.Harass.yPos:Value()
+        if Warwick.Harass.T:Value() then
+		    Draw.Text("HARASS ON", size, textPos.x + xPos, textPos.y + yPos, Draw.Color(255, 000, 255, 000))
+	    else
+            Draw.Text("HARASS OFF", size, textPos.x + xPos, textPos.y + yPos, Draw.Color(255, 255, 000, 000))
+        end
+    end
+    if Warwick.Draw.Clear.Text:Value() then
+        local size = Warwick.Draw.Clear.Size:Value()
+	    local xPos = Warwick.Draw.Clear.xPos:Value()
+	    local yPos = Warwick.Draw.Clear.yPos:Value()
+        if Warwick.Clear.T:Value() then
+		    Draw.Text("CLEAR ON", size, textPos.x + xPos, textPos.y + yPos, Draw.Color(255, 000, 255, 000))
+	    else
+            Draw.Text("CLEAR OFF", size, textPos.x + xPos, textPos.y + yPos, Draw.Color(255, 255, 000, 000))
+        end
+    end
+end
+
 class "Zyra"
 
 function Zyra:__init()
@@ -2210,7 +2365,7 @@ function Utility:Cleanse()
     end
 end
 
-local Heroes = {"Ahri","DrMundo","Jayce","Soraka","Tryndamere","Vladimir","Zyra"}
+local Heroes = {"Ahri","DrMundo","Jayce","Soraka","Tryndamere","Vladimir","Warwick","Zyra"}
 local Heroloaded = false
 local ActivatorLoaded = false
 Callback.Add("Load", function()
